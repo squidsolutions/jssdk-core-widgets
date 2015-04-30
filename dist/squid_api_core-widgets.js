1a00b60a7571648db93bc7045f84f92021675af1
olivier <oba@squid>
2015-04-30 18:16:19 +0200

    JSSDK-50

--- dist/squid_api_core-widgets.js
+++ dist/squid_api_core-widgets.js
@@ -28,6 +28,65 @@ function program3(depth0,data) {
   else { return ''; }
   });
 
+this["squid_api"]["template"]["squid_api_pagination"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression, self=this;
+
+function program1(depth0,data) {
+  
+  var buffer = "", stack1;
+  buffer += "\n		<li class=\"previous\" data-id=\""
+    + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.prev)),stack1 == null || stack1 === false ? stack1 : stack1.id)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "\"><a href=\"#\"><i class=\"fa fa-arrow-left\"></i></a></li>\n		";
+  return buffer;
+  }
+
+function program3(depth0,data) {
+  
+  var buffer = "", stack1, helper;
+  buffer += "\n		<li data-id=\"";
+  if (helper = helpers.id) { stack1 = helper.call(depth0, {hash:{},data:data}); }
+  else { helper = (depth0 && depth0.id); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
+  buffer += escapeExpression(stack1)
+    + "\"";
+  stack1 = helpers['if'].call(depth0, (depth0 && depth0.selected), {hash:{},inverse:self.noop,fn:self.program(4, program4, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "><a href=\"#\">";
+  if (helper = helpers.id) { stack1 = helper.call(depth0, {hash:{},data:data}); }
+  else { helper = (depth0 && depth0.id); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
+  buffer += escapeExpression(stack1)
+    + "</a></li>\n		";
+  return buffer;
+  }
+function program4(depth0,data) {
+  
+  
+  return " class=\"active\" ";
+  }
+
+function program6(depth0,data) {
+  
+  var buffer = "", stack1;
+  buffer += "\n		<li class=\"next\"  data-id=\""
+    + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.next)),stack1 == null || stack1 === false ? stack1 : stack1.id)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "\"><a  href=\"#\"><i class=\"fa fa-arrow-right\"></i></a></li>\n		";
+  return buffer;
+  }
+
+  buffer += "<div class=\"pagination\">\n	<ul>\n		";
+  stack1 = helpers['if'].call(depth0, (depth0 && depth0.prev), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n		";
+  stack1 = helpers.each.call(depth0, (depth0 && depth0.pages), {hash:{},inverse:self.noop,fn:self.program(3, program3, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n		";
+  stack1 = helpers['if'].call(depth0, (depth0 && depth0.next), {hash:{},inverse:self.noop,fn:self.program(6, program6, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\n	</ul>\n</div>";
+  return buffer;
+  });
+
 this["squid_api"]["template"]["squid_api_selector"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
@@ -265,6 +324,79 @@ function program5(depth0,data) {
     });
 
     return LoginView;
+}));
+
+(function (root, factory) {
+    root.squid_api.view.PaginationView = factory(root.Backbone, root.squid_api);
+}(this, function (Backbone, squid_api) {
+
+    var View = Backbone.View.extend({
+
+        config : null,
+        
+        pagesRangeSize : 10,
+
+        initialize : function(options) {
+            this.config = options.config || squid_api.model.config;
+            this.config.on("change:startIndex", this.render, this);
+            this.config.on("change:pageLength", this.render, this);
+            this.model.on("change:results", this.render, this);
+        },
+        
+        events : { 
+            "click li" : function(event) {
+                event.preventDefault();
+                var pageId = $(event.currentTarget).data("id");
+                var pageSize = this.config.get("maxResults");
+                this.config.set("startIndex", pageId * pageSize);
+            }
+        },
+
+        render : function() {
+            var pageSize = this.config.get("maxResults");
+            var startIndex = this.config.get("startIndex");
+            var results = this.model.get("results");
+            
+            if (results) {
+                var totalSize = results.totalSize;
+                var currentPageId = Math.floor(startIndex/pageSize);
+                var totalPages = Math.ceil(totalSize/pageSize);
+                var firstPageToDisplay = Math.ceil(currentPageId / this.pagesRangeSize);
+                    
+                var pages = [];
+                // prev
+                var prev;
+                if (firstPageToDisplay>0) {
+                    prev = { "id" : firstPageToDisplay-1};
+                }
+                // pages
+                var pageId = 0;
+                for (var i=0; ((i<this.pagesRangeSize) && (i<totalPages)); i++) {
+                    pageId = firstPageToDisplay+i;
+                    if (pageId == currentPageId) {
+                        selected = true;
+                    }
+                    pages.push({ "id" : pageId, "selected" :  selected});
+                }
+                // next
+                var next;
+                if (pageId<totalPages) {
+                    next = { "id" : pageId+1};
+                }
+                
+                var html = squid_api.template.squid_api_pagination({
+                    "prev" : prev,
+                    "pages" : pages,
+                    "next" : next
+                });
+                    
+                this.$el.html(html);
+            }
+
+        }
+    });
+
+    return View;
 }));
 
 (function (root, factory) {
